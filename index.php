918cd87a1f1e169f53093aee74155a7a877ce209
Fernando Santos <94801105+Fernandonds@users.noreply.github.com>
2021-11-22 20:13:59 -0300

    php

--- index.php
+++ index.php
@@ -52,8 +52,24 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, optio perspiciatis aliquam eligendi molestiae beatae nobis. Quos, maiores voluptate? Culpa, alias. Eius recusandae velit eligendi dolores cum hic quae vitae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem porro odit officia aliquid et molestiae iusto exercitationem quis, dicta explicabo deleniti quod quaerat at asperiores ducimus eaque perferendis nesciunt illum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, veniam! Quos modi, in voluptatibus vitae corrupti minus accusantium aspernatur repellendus perferendis est quas nesciunt soluta officiis sed tempora earum facere.</p>
                         <button class='btn-articles'>Ler mais...</button>
                         <div class='border-bottom'></div>
-                        </div><!--box-->
+                        </div>
+                        <!--box-->
+            
+            <?php
 
+                for($c = 1; $c < 5; $c++){
+                    echo "
+                    <div class='box'>
+                    <img src='images/imagem-teste.jpg'>
+                    <h3>Título</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, optio perspiciatis aliquam eligendi molestiae beatae nobis. Quos, maiores voluptate? Culpa, alias. Eius recusandae velit eligendi dolores cum hic quae vitae. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem porro odit officia aliquid et molestiae iusto exercitationem quis, dicta explicabo deleniti quod quaerat at asperiores ducimus eaque perferendis nesciunt illum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, veniam! Quos modi, in voluptatibus vitae corrupti minus accusantium aspernatur repellendus perferendis est quas nesciunt soluta officiis sed tempora earum facere.</p>
+                    <button class='btn-articles'>Ler mais...</button>
+                    <div class='border-bottom'></div>
+                    </div>
+                    <!--box-->
+                    ";
+                }
+            ?>
     
             </div><!--box-articles-->
     
